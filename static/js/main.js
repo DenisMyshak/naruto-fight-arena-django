2d28de98774738fbaf31e70b87b8c2b8e41ae119
Денис Мышак <papakicaden8@gmail.com>
2024-01-21 00:41:12 +0100

    Add Carousel

---
+++ static/js/main.js
@@ -0,0 +1,19 @@
+$('.set-bg').each(function () {
+    var bg = $(this).data('setbg');
+    $(this).css('background-image', 'url(' + bg + ')');
+});
+
+var hero_s = $(".hero__slider");
+hero_s.owlCarousel({
+    loop: true,
+    margin: 0,
+    items: 1,
+    dots: true,
+    nav: true,
+    navText: ["<span class='arrow_carrot-left'></span>", "<span class='arrow_carrot-right'></span>"],
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    smartSpeed: 1200,
+    autoHeight: false,
+    mouseDrag: false
+});
