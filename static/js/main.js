34f8e80ddbb13435865357b504cab9ca21878b3a
Денис Мышак <papakicaden8@gmail.com>
2024-01-21 15:40:23 +0100

    Fix navigation arrows

--- static/js/main.js
+++ static/js/main.js
@@ -10,7 +10,10 @@ hero_s.owlCarousel({
     items: 1,
     dots: true,
     nav: true,
-    navText: ["<span class='arrow_carrot-lefthsfle'></span>", "<span class='arrow_carrot-right'></span>"],
+    navText: [
+        "<span class='arrow_carrot-left'><img src='static/img/arrow_left_nav_icon.svg' /></span>",
+        "<span class='arrow_carrot-right'><img src='static/img/arrow_right_nav_icon.svg' /></span>"
+    ],
     animateOut: 'fadeOut',
     animateIn: 'fadeIn',
     smartSpeed: 1200,
