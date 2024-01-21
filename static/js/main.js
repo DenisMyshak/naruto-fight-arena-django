48da21f1ded05f5ed2478257cdbd5d3639263dbe
Денис Мышак <papakicaden8@gmail.com>
2024-01-21 14:47:28 +0100

    LALA

--- static/js/main.js
+++ static/js/main.js
@@ -10,7 +10,7 @@ hero_s.owlCarousel({
     items: 1,
     dots: true,
     nav: true,
-    navText: ["<span class='arrow_carrot-left'></span>", "<span class='arrow_carrot-right'></span>"],
+    navText: ["<span class='arrow_carrot-lefthsfle'></span>", "<span class='arrow_carrot-right'></span>"],
     animateOut: 'fadeOut',
     animateIn: 'fadeIn',
     smartSpeed: 1200,
