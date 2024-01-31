c998a1b0b0edfc3d256f569431b13ca51b2dd433
Денис Мышак <papakicaden8@gmail.com>
2024-01-31 18:28:59 +0100

    parser selenium(works) but deals only with images

--- jutsu_character_parser.py
+++ jutsu_character_parser.py
@@ -30,6 +30,7 @@ def update_character_battle_image(character_name, image_link, cursor):
     if search_characters_with_name(character_name, cursor):
         sql = "UPDATE characters SET battle_image = %s WHERE character_name = %s"
         cursor.execute(sql, (image_link, character_name))
+        mydb.commit()
     else:
         unmatched_characters.append(character_name)
 
@@ -44,12 +45,9 @@ def scrap_image_and_move_to_db(soup, character_name, cursor):
 def create_characters_link_list():
     url = "https://jut.su/ninja"
     
-    try:
-        driver.get(url)
-        driver.implicitly_wait(3)
-        page_source = driver.page_source
-    finally:
-        driver.quit()
+    driver.get(url)
+    driver.implicitly_wait(3)
+    page_source = driver.page_source
 
     soup = BeautifulSoup(page_source, "html.parser")
     ninja_list_ul = soup.find('ul', class_='ninja_list')
@@ -60,26 +58,26 @@ def create_characters_link_list():
 
     return valid_character_links
 
-def browse_jutsu_and_charac_image(seed_url, page_number=1):
-    print("start parsing" + seed_url)
+def browse_jutsu_and_charac_image(seed_url, source_url, page_number=1):
+    if page_number > 1:
+        seed_url = seed_url.format(str(page_number))
     try:
-        driver.get(seed_url.format(page_number) if page_number > 1 else seed_url)
+        driver.get(seed_url)
+        print("start parsing " + seed_url)
         page_source = driver.page_source
         soup = BeautifulSoup(page_source, "html.parser")
 
         if page_number == 1:
             scrap_image_and_move_to_db(soup, extract_character_name(seed_url), cursor)
 
-        next_button = soup.find("div", id="navigation").find('a', text="Далее")
+        next_button = soup.find("div", id="navigation").find('a', string="Далее")
         if next_button:
-            seed_url += "page/{}/"
+            seed_url = source_url + "page/{}"
             page_number += 1
-            browse_jutsu_and_charac_image(seed_url, page_number)
+            browse_jutsu_and_charac_image(seed_url, source_url, page_number)
     except Exception as e:
         print(e)
         return e
-    finally:
-        driver.quit()
 
 def extract_character_name(seed_url):
     pattern = r'/ninja/(\w+)/'
@@ -88,11 +86,12 @@ def extract_character_name(seed_url):
     return unquote(match.group(1).replace("_", " ")) if match else None
 
 def main():
-    global driver, cursor, unmatched_characters
+    global driver, cursor, unmatched_characters, mydb
     unmatched_characters = []
 
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--log-level=3')
     driver = webdriver.Chrome(options=chrome_options)
 
     mydb = create_database_connection()
@@ -101,7 +100,7 @@ def main():
     try:
         character_links = create_characters_link_list()
         for link in character_links:
-            browse_jutsu_and_charac_image(link)
+            browse_jutsu_and_charac_image(link, link)
     finally:
         cursor.close()
         mydb.close()
